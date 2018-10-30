589385fdb94ed5abef3018b17976ee84cdf332cb
ebb94f53au <137706770@qq.com>
2018-10-30 17:28:22 +0800

    update

--- run.py
+++ run.py
@@ -1,7 +1,9 @@
 #coding=utf-8
 from proxypool.schedule import Schedule
 from proxypool.api import app
-
+# s=Schedule()
+# s.run()
+#web 服务器运行
 if __name__ == '__main__':
     s=Schedule()
     s.run()
